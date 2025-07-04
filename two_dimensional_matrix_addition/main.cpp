33b6e8a4f457ec5046039f1b52d4a58ef24721a8
Mehmet Bozhayadar <mehmetbozhaydar@hotmail.com>
2025-07-04 06:24:06 +0300

    seventh project start

---
+++ two_dimensional_matrix_addition/main.cpp
@@ -0,0 +1,18 @@
+/*
+* İki  Boyutlu Dinamik Matrislerle Toplama İşlemi
+*  
+* Kullanıcıdan satır ve sütün sayısı aldıktan sonra , iki matris oluşturup bu 
+* matrislerin elemanları yine kullanıcıdan alınıcak. Ardından bu iki matrisi eleman bazında
+* toplayacak ve sonuçlar ekrana yazdırılacak.
+* 
+* Not: Tüm  bellek kullanıımından sonra belleği temizlemeyi unutma.Bu proje, pointer'ların ve 
+* dinamik bellek yönetiminin kullanımını pekiştirmek için uygulama
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
